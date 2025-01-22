d7f09770d07d088a20e2a0db96a36346726958cd
isanchez03 <strawberrymoonsky02@gmail.com>
2025-01-22 20:12:28 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: https://isanchez03.github.io,
+  site: 'https://isanchez03.github.io',
     ? 'https://astro-shadcn-ui-template.vercel.app'
     : 'http://localhost:4321',
   integrations: [
